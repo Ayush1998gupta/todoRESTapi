c8827a7f0eaa791697f01957f3e5b5d1ef28b3c5
Ayush Gupta <aayush199813@gmail.com>
2023-04-21 21:06:10 +0530

    pagination added

--- controllers/task.js
+++ controllers/task.js
@@ -19,14 +19,20 @@ export const createTask = async (req, res, next) => {
 export const updateTask = async (req, res, next) => {
   try {
     const task = await Task.findById(req.params.taskId).exec();
-    if (!task) return next(createError({ status: 404, message: 'Task not found' }));
-    if (task.user.toString() !== req.user.id) return next(createError({ status: 401, message: "It's not your todo." }));
+    if (!task)
+      return next(createError({ status: 404, message: 'Task not found' }));
+    if (task.user.toString() !== req.user.id)
+      return next(createError({ status: 401, message: "It's not your todo." }));
 
-    const updatedTask = await Task.findByIdAndUpdate(req.params.taskId, {
-      title: req.body.title,
-      status: req.body.status,
-      descriptions: req.body.descriptions,
-    }, { new: true });
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.taskId,
+      {
+        title: req.body.title,
+        status: req.body.status,
+        descriptions: req.body.descriptions,
+      },
+      { new: true }
+    );
     return res.status(200).json(updatedTask);
   } catch (err) {
     return next(err);
@@ -37,13 +43,16 @@ export const getAllTasks = async (req, res, next) => {
   try {
     const { title, descriptions } = req.query;
     const queryObject = {};
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 2;
+    const skip = (page - 1) * limit;
     if (title) {
       queryObject.title = { $regex: title, $options: 'i' };
     }
     if (descriptions) {
       queryObject.descriptions = { $regex: descriptions, $options: 'i' };
     }
-    const tasks = await Task.find(queryObject);
+    const tasks = await Task.find(queryObject).skip(skip).limit(limit);
     return res.status(200).json(tasks);
   } catch (err) {
     return next(err);
@@ -52,7 +61,15 @@ export const getAllTasks = async (req, res, next) => {
 
 export const getCurrentUserTasks = async (req, res, next) => {
   try {
-    const tasks = await Task.find({ user: req.user.id });
+    const { title, descriptions } = req.query;
+    const queryObject = { user: req.user.id };
+    if (title) {
+      queryObject.title = { $regex: title, $options: 'i' };
+    }
+    if (descriptions) {
+      queryObject.descriptions = { $regex: descriptions, $options: 'i' };
+    }
+    const tasks = await Task.find(queryObject);
     return res.status(200).json(tasks);
   } catch (err) {
     return next(err);
